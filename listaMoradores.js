3f395d3697da9399558276041e4f6846a9fe57c3
Sidney Rafael Mantovani Ribeiro <sidneyrafael191@gmail.com>
2022-12-18 21:03:36 -0300

    programa de consulta de moradores em condominio residencial, primeiro commit

---
+++ listaMoradores.js
@@ -0,0 +1,209 @@
+const moradores =
+    [
+        {
+            "id": 1,
+            "bloco": "D",
+            "apto": 12,
+            "morador": "Ednamar Costa"
+        },
+        {
+            "id": 2,
+            "bloco": "D",
+            "apto": 12,
+            "morador": "Paulo Cesar"
+        },
+        {
+            "id": 3,
+            "bloco": "D",
+            "apto": 13,
+            "morador": "Luiz Felipe dos Reis"
+
+        },
+        {
+            "id": 4,
+            "bloco": "D",
+            "apto": 13,
+            "morador": "Caroline Patricia Biazoto"
+        },
+        {
+            "id": 5,
+            "bloco": "D",
+            "apto": 14,
+            "morador": "Negival Araujo Mistro"
+        },
+        {
+            "id": 6,
+            "bloco": "D",
+            "apto": 14,
+            "morador": "Aline Vieira Mistro Souza"
+        },
+        {
+            "id": 7,
+            "bloco": "D",
+            "apto": 22,
+            "morador": "Geraldo Ribeiro"
+        },
+        {
+            "id": 8,
+            "bloco": "D",
+            "apto": 22,
+            "morador": "Daniela Andrade"
+        },
+        {
+            "id": 9,
+            "bloco": "D",
+            "apto": 23,
+            "morador": "Kaique Vinicius"
+        },
+        {
+            "id": 10,
+            "bloco": "D",
+            "apto": 23,
+            "morador": "Eric Matheus"
+        },
+        {
+            "id": 11,
+            "bloco": "D",
+            "apto": 24,
+            "morador": "Edevaldo de Oliveira"
+        },
+        {
+            "id": 12,
+            "bloco": "D",
+            "apto": 24,
+            "morador": "Amanda Lauriano"
+
+        },
+        {
+            "id": 13,
+            "bloco": "D",
+            "apto": 31,
+            "morador": "Gustavo Arantes"
+        },
+        {
+            "id": 14,
+            "bloco": "D",
+            "apto": 31,
+            "morador": "Nathaly Motta Reale"
+        },
+        {
+            "id": 15,
+            "bloco": "D",
+            "apto": 34,
+            "morador": "Leandro Rafael Rotuli"
+        },
+        {
+            "id": 16,
+            "bloco": "D",
+            "apto": 34,
+            "morador": "Laísse Regina Rodrigues"
+
+        },
+        {
+            "id": 17,
+            "bloco": "D",
+            "apto": 34,
+            "morador": "Yuri Felipe do Prado"
+
+        },
+        {
+            "id": 18,
+            "bloco": "D",
+            "apto": 41,
+            "morador": "Lelia Pithon Marini"
+
+        },
+        {
+            "id": 19,
+            "bloco": "D",
+            "apto": 42,
+            "morador": "Ivone dos Santos"
+
+        },
+        {
+            "id": 20,
+            "bloco": "D",
+            "apto": 43,
+            "morador": "Vinicius Moreira"
+
+        },
+        {
+            "id": 21,
+            "bloco": "D",
+            "apto": 52,
+            "morador": "Maria Jose dos Santos"
+
+        },
+        {
+            "id": 22,
+            "bloco": "D",
+            "apto": 52,
+            "morador": "Aillana dos Santos"
+
+        },
+        {
+            "id": 23,
+            "bloco": "D",
+            "apto": 53,
+            "morador": "Maria Lucia"
+
+        },
+        {
+            "id": 24,
+            "bloco": "D",
+            "apto": 54,
+            "morador": "Caio Mendes"
+
+        },
+        {
+            "id": 25,
+            "bloco": "D",
+            "apto": 61,
+            "morador": "Luiz Silvestre Netto"
+
+        },
+        {
+            "id": 26,
+            "bloco": "D",
+            "apto": 61,
+            "morador": "Edna Rampori Silvestre"
+
+        },
+        {
+            "id": 27,
+            "bloco": "D",
+            "apto": 62,
+            "morador": "Neide Francatto"
+
+        },
+        {
+            "id": 28,
+            "bloco": "D",
+            "apto": 63,
+            "morador": "João Batista Henrique"
+
+        },
+        {
+            "id": 29,
+            "bloco": "D",
+            "apto": 63,
+            "morador": "Fatima Maria Henrique"
+
+        },
+        {
+            "id": 30,
+            "bloco": "D",
+            "apto": 64,
+            "morador": "Ivonette Pereira Vinicius"
+
+        },
+        {
+            "id": 31,
+            "bloco": "D",
+            "apto": 64,
+            "morador": "Brunna Pereira Nicolete"
+
+        }
+    ]
+
+export default moradores
